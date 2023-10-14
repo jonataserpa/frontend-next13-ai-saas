85b7b0b5d02e7c0c54e5838a07e3f7df83c40259
Jonata Serpa <jonataserpa@gmail.com>
2023-10-14 19:36:49 -0300

    feat: add sqllite

---
+++ prisma/migrations/20231014223524_create_structure_db/migration.sql
@@ -0,0 +1,30 @@
+-- CreateTable
+CREATE TABLE "UserApiLimit" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    "count" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "UserSubscription" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    "stripe_customer_id" TEXT,
+    "stripe_subscription_id" TEXT,
+    "stripe_price_id" TEXT,
+    "stripe_current_period_end" DATETIME
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserApiLimit_userId_key" ON "UserApiLimit"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserSubscription_userId_key" ON "UserSubscription"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserSubscription_stripe_customer_id_key" ON "UserSubscription"("stripe_customer_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserSubscription_stripe_subscription_id_key" ON "UserSubscription"("stripe_subscription_id");
