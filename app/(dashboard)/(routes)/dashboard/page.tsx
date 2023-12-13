1676a2e0640c553647a3104fd6e5f30572a97e9c
Jonata Serpa <jonataserpa@gmail.com>
2023-12-13 14:54:50 -0300

    fix: adjust route layout

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -15,10 +15,10 @@ export default function HomePage() {
     <div>
       <div className="mb-8 space-y-4">
         <h2 className="text-2xl md:text-4xl font-bold text-center">
-          Explore the power of AI
+          Aproceite o poder da AI
         </h2>
         <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
-          Chat with the smartest AI - Experience the power of AI
+          Chat inteligente com AI - os poderes da AI
         </p>
       </div>
       <div className="px-4 md:px-20 lg:px-32 space-y-4">
