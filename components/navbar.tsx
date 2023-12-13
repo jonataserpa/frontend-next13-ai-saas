4138816448692ae44ca956449d196cf8ebbc312a
Jonata Serpa <jonataserpa@gmail.com>
2023-12-13 14:45:56 -0300

    fix: adjust layout

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,12 +5,12 @@ import { getApiLimitCount } from "@/lib/api-limit";
 import { checkSubscription } from "@/lib/subscription";
 
 const Navbar = async () => {
-  const apiLimitCount = await getApiLimitCount();
-  const isPro = await checkSubscription();
+  //const apiLimitCount = await getApiLimitCount();
+  //const isPro = await checkSubscription();
 
   return ( 
     <div className="flex items-center p-4">
-      <MobileSidebar isPro={isPro} apiLimitCount={apiLimitCount} />
+      <MobileSidebar isPro={false} apiLimitCount={1} />
       <div className="flex w-full justify-end">
         <UserButton afterSignOutUrl="/" />
       </div>
